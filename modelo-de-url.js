1f2edd01bcffceaa236b5f59804d00d1e6551a6a
DouglasAmarelom <d.lopes.nascimento@gmail.com>
2018-07-08 09:14:26 -0300

    Commitando para ter a função getParam no histórico

---
+++ modelo-de-url.js
@@ -0,0 +1,10 @@
+
+	// const urlLoja       = 'https://loja.compracerta.com.br';
+	// let   paramSearch   = 'lava-loucas-brastemp-14-servicos-blf14ae-2003643/p';
+	// let   paramLogin    = 'douglas@jussi.com.br';
+	// let   paramSource   = 'whirlpool';
+	// let   paramMedium   = 'email_blast';
+	// let   paramCampaign = '20180627-preco-lavalouca';
+	// let   paramCp       = 'whp_emkt';
+	// let   paramPc       = 'email_blast';
+	// let   paramEmail    = paramLogin;
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,14 +2,14 @@
 	'use strict';
 
 	// Variables
-	const $formUrl  = document.querySelector('.url-form');
+	const $form  = document.querySelector('.url-form');
 	const $body     = document.querySelector('body');
 	const $url      = document.querySelector('.url');
 	const $reset    = document.querySelector('input[type="reset"]');
-	let   inputs    = $formUrl.querySelectorAll('[class*="url-form__"]');
-	let   lojaTitle = document.querySelector('.text-loja');
+	const $inputs    = $form.querySelectorAll('[class*="url-form__"]');
+	const $store = document.querySelector('.text-loja');
 	let urlLoja,
-		paramSearch,
+		paramProducts,
 		paramLogin,
 		paramSource,
 		paramMedium,
@@ -19,47 +19,22 @@
 		paramEmail,
 		completeUrl;
 
-	// const urlLoja       = 'https://loja.compracerta.com.br';
-	// let   paramSearch   = 'lava-loucas-brastemp-14-servicos-blf14ae-2003643/p';
-	// let   paramLogin    = 'douglas@jussi.com.br';
-	// let   paramSource   = 'whirlpool';
-	// let   paramMedium   = 'email_blast';
-	// let   paramCampaign = '20180627-preco-lavalouca';
-	// let   paramCp       = 'whp_emkt';
-	// let   paramPc       = 'email_blast';
-	// let   paramEmail    = paramLogin;
-
-
 	// Atualiza o valor das variáveis com o valor dos campos
 	function updateVariables() {
-		urlLoja       = $formUrl.loja.options[$formUrl.loja.selectedIndex].getAttribute('data-url');
-		paramSearch   = $formUrl.produto.value;
-		paramLogin    = $formUrl.login.value;
-		paramSource   = $formUrl.utmSource.value;
-		paramMedium   = $formUrl.utmMedium.value;
-		paramCampaign = $formUrl.utmCampaign.value;
-		paramCp       = $formUrl.utmiCp.value;
-		paramPc       = $formUrl.utmiPc.value;
+		urlLoja       = $form.loja.options[$form.loja.selectedIndex].getAttribute('data-url');
+		paramProducts = $form.produto.value;
+		paramLogin    = $form.login.value;
+		paramSource   = $form.utmSource.value;
+		paramMedium   = $form.utmMedium.value;
+		paramCampaign = $form.utmCampaign.value;
+		paramCp       = $form.utmiCp.value;
+		paramPc       = $form.utmiPc.value;
 		paramEmail    = paramLogin;
-	}
-
-	// Define o template com base na loja escolhida
-	function urlTemplate() {
-		let loja = $formUrl.loja.value;
-
-		completeUrl = `${urlLoja + paramSearch}?utm_source=${paramSource}&utm_medium=${paramMedium}&utm_campaign=${paramCampaign}&utmi_cp=${paramCp}&utmi_pc=${paramPc}`;
-
-		if (loja === 'Compra Certa (Corp)' || loja === 'Compra Certa (Colab)') {
-			completeUrl = `${urlLoja + paramSearch}?login=${paramLogin}&ReturnUrl=/${paramSearch}?utm_source=${paramSource}&utm_medium=${paramMedium}&utm_campaign=${paramCampaign}&utmi_cp=${paramCp}&utmi_pc=${paramPc}&email=${paramEmail}`;
-		}
-
-
-		$url.textContent = completeUrl;
 	}
 
 	// Faz a validação do formulário
 	function formValidation(instruction) {
-		inputs.forEach(( item ) => {
+		$inputs.forEach(( item ) => {
 			if ( instruction === 'addError' ) {
 				if (item.value === '' || item.value === undefined) {
 					item.classList.add('error');
@@ -72,63 +47,108 @@
 		});
 	}
 
+	// Define o template com base na loja escolhida
+	function generateUrl() {
+		let loja = $form.loja.value;
+
+		completeUrl = `${urlLoja + paramProducts}?utm_source=${paramSource}&utm_medium=${paramMedium}&utm_campaign=${paramCampaign}&utmi_cp=${paramCp}&utmi_pc=${paramPc}`;
+
+		if (loja === 'Compra Certa (Corp)' || loja === 'Compra Certa (Colab)') {
+			completeUrl = `${urlLoja + paramProducts}?login=${paramLogin}&ReturnUrl=/${paramProducts}?utm_source=${paramSource}&utm_medium=${paramMedium}&utm_campaign=${paramCampaign}&utmi_cp=${paramCp}&utmi_pc=${paramPc}&email=${paramEmail}`;
+		}
+
+		$url.textContent = completeUrl;
+	}
+
+
+	// Verifica os campos que tem informação e registra
+	// parâmetros e valores no objeto "data"
+	// function getParam() {
+	// 	let data = [];
+
+	// 	document.querySelectorAll('input[type=text]').forEach(( item, index ) => {
+	// 		if ( item.value !== '' && item.value !== undefined ) {
+	// 			data.push(
+	// 				{
+	// 					"param": item.getAttribute('data-param'),
+	// 					"val": item.value
+	// 				}
+	// 			);
+	// 		}
+	// 	});
+
+
+	// 	data.map( item => {
+	// 		if (item.param === 'login') {
+	// 			item.param = `ReturnUrl=/${item.param}`;
+	// 		}
+	// 	});
+
+	// 	data = data.map( item => `${item.param}=${item.val}` );
+
+	// 	console.log(data);
+
+	// 	return data;
+	// }
+
+	// Reset
 	function resetForm() {
 		formValidation('removeError');
 		$body.removeAttribute('class');
 		$url.textContent = '';
 	}
 
-	// Campos do formulário
-	$formUrl.addEventListener('change', () => {
+	$reset.addEventListener('click', resetForm);
+
+
+	// Assiste as mudanças nos campos do formulário
+	$form.addEventListener('change', () => {
 		updateVariables();
 		formValidation('removeError');
 	});
 
-	function getParam() {
-		let data = [];
-
-		inputs.forEach(( item, index ) => {
-			if ( item.value !== '' || item.value !== undefined ) {
-				data.push(
-					{
-						"param": item.getAttribute('data-param'),
-						"val": item.value
-					}
-				);
-			}
-		});
-
-		console.log( data );
-	}
-
-	// Reset
-	$reset.addEventListener('click', resetForm);
 
 	// Select do formulário
-	$formUrl.loja.addEventListener('change', function() {
+	$form.loja.addEventListener('change', function() {
 		let text = this.value;
-		lojaTitle.textContent = text !== 'Selecione a loja' ? text : '';
+		$store.textContent = text !== 'Selecione a loja' ? text : '';
 
 		text = text.toLocaleLowerCase().replace(/-/gm, '').replace(/\s+/gm, '-');
 
 		$body.setAttribute('class', `${text}`);
 	});
 
+
 	// Submit do formulário
-	$formUrl.addEventListener('submit', function(e) {
+	$form.addEventListener('submit', function(e) {
 		e.preventDefault();
 
 		// Atualiza a URL final
-		if ($formUrl.loja.value !== 'Selecione a loja') {
-			urlTemplate();
+		if ($form.loja.value !== 'Selecione a loja') {
+			generateUrl();
 		}
 		else {
 			resetForm();
 		}
 
 		formValidation('addError');
-
-		getParam();
 	});
+
+
+
+
+
+	document.querySelector('#produto').value = '/geladeira/p, /fogao/p, 102030';
+	function getProducts() {
+		let products = $form.produto.value;
+		products = products.split(',');
+
+
+		console.log( products );
+		console.log( Object.prototype.toString.call(products) );
+	}
+
+	getProducts();
+
 
 })();
